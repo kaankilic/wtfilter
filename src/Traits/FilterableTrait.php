df7a0c6a42b5abb8a3740a6eb2802b052c87c322
Kaan Kılıç <kaankilic@users.noreply.github.com>
2017-10-03 10:09:57 +0300

    Update FilterableTrait.php

--- src/Traits/FilterableTrait.php
+++ src/Traits/FilterableTrait.php
@@ -1,13 +1,33 @@
 <?php 
 namespace Kaankilic\WTFilter\Traits;
 use Kaankilic\WTFilter\Facades\WTFilter;
-trait FilterableTrait
+trait FilterableWords
 {
     public function filterable(){
-      return [];
+      return [
+          "sources" => ["title"],
+          "flag" => "has_profanity"
+      ];
     }
     protected static function boot(){
         parent::boot();
+        static::creating(function($model){
+           self::checkProfanity();
+        });
+        static::updating(function($model){
+           self::checkProfanity();
+        });
     }
+    public static function checkProfanity(){
+        $config = self::filterable();
+        if (is_array($config["sources"])){
+            foreach($source in $sources){
+                $model->{{$source}} = WTFilter::filter($model->{{$source}});
+            }
+        }
+        if(is_string($config["sources"])){
+            $model->{{$source}} = WTFilter::filter($model->{{$source}});
+        }
+    }   
 }
 ?>
